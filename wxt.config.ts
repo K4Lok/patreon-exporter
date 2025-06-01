f9675351e685a5a8a5db70dfbeca23ad5377b223
ImK4Lok <samkalok107@gmail.com>
2025-06-01 15:35:51 +0800

    init: project created by wxt

---
+++ wxt.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
+  manifest: {
+    version: "0.0.1",
+    manifest_version: 3,
+    name: "Patreon Exporter",
+    permissions: ["activeTab", "downloads", "storage"],
+  }
+});
